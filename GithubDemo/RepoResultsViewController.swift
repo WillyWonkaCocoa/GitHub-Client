b357b4f4e60d838855bea53e8058f8e8f77969bc
WillyWonkaCocoa <wgao1@MacBook-Pro.local>
2016-02-18 21:38:03 -0500

    Initial GitHub Lab

---
+++ GithubDemo/RepoResultsViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  GithubDemo
+//
+//  Created by Nhan Nguyen on 5/12/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+import UIKit
+import MBProgressHUD
+
+// Main ViewController
+class RepoResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var starLabel: UILabel!
+    @IBOutlet weak var ownerLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var searchBar: UISearchBar!
+    var searchSettings = GithubRepoSearchSettings()
+    @IBOutlet weak var tableView: UITableView!
+
+    var repos: [GithubRepo]!
+    
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("repoCell", forIndexPath: indexPath) as! repoCell
+        let repo = repos![indexPath.row]
+        
+        //cell.descriptionLabel.text = repo[]
+        
+        tableView.reloadData()
+        return cell
+    }
+    
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        // Initialize the UISearchBar
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+
+        // Add SearchBar to the NavigationBar
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+
+        // Perform the first search when the view controller first loads
+        doSearch()
+    }
+
+    // Perform the search.
+    private func doSearch() {
+
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+
+        // Perform request to GitHub API to get the list of repositories
+        GithubRepo.fetchRepos(searchSettings, successCallback: { (newRepos) -> Void in
+            self.repos = newRepos as [GithubRepo]!
+
+            // Print the returned repositories to the output window
+            for repo in newRepos {
+                print(repo)
+            }   
+            self.tableView.reloadData()
+            MBProgressHUD.hideHUDForView(self.view, animated: true)
+            }, error: { (error) -> Void in
+                print(error)
+        })
+    }
+}
+
+// SearchBar methods
+extension RepoResultsViewController: UISearchBarDelegate {
+
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true;
+    }
+
+    func searchBarShouldEndEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true;
+    }
+
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchSettings.searchString = searchBar.text
+        searchBar.resignFirstResponder()
+        doSearch()
+    }
+}
---
+++ GithubDemo/repoCell.swift
@@ -0,0 +1,24 @@
+//
+//  repoCell.swift
+//  GithubDemo
+//
+//  Created by wgao1 on 2/18/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class repoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
